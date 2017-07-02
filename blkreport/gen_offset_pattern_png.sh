f0788523ecdb1522fddfa8e49d0e18b7706b55ce
Bean Li <beanli.coder@gmail.com>
2017-07-02 22:26:59 +0800

    add offset pattern

---
+++ blkreport/gen_offset_pattern_png.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+usage()
+{
+    echo $(basename $0) " [device_name]"
+}
+if [ $# != 1 ]; then 
+  usage
+  exit 1
+fi
+
+device=$1
+
+if [ x"$device" = x"" ];then
+  echo "device is null"
+  exit 1 
+fi
+
+
+
+blkparse -i $device -d $device.blktrace.bin  > /dev/null
+
+btt -i $device.blktrace.bin -B $device.offset > /dev/null 
+filename=`ls ${device}.offset*_c.dat`
+
+awk '{print $1,$2,$3-$2}' $filename >${filename}.new
+
+echo "
+set terminal pngcairo lw 2
+set title \"$device Generate Block access\"
+set xlabel \"Time (second)\"
+set ylabel \"Block Number\"
+set zlabel \"# Blocks Per IO\"
+set grid
+set output '${device}_offset_pattern.png'
+splot \"${filename}.new\" u 1:2:3 w l ls 1 title \"Offset\" 
+set output
+set terminal wxt
+" | /usr/bin/gnuplot
+
