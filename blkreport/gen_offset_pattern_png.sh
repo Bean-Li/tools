de292bfb45822bbebb9dd720c627b83260516ee2
Bean Li <bean_li@bigtera.com.cn>
2017-07-05 19:59:29 -0700

    generate iosize historgram

--- blkreport/gen_offset_pattern_png.sh
+++ blkreport/gen_offset_pattern_png.sh
@@ -33,6 +33,7 @@ set title \"$device Generate Block access\"
 set xlabel \"Time (second)\"
 set ylabel \"Block Number\"
 set zlabel \"# Blocks Per IO\"
+set xtics rotate by 60
 set grid
 set output '${device}_offset_pattern.png'
 splot \"${filename}.new\" u 1:2:3 w l ls 1 title \"Offset\" 
@@ -51,3 +52,27 @@ set output
 set terminal wxt
 " | /usr/bin/gnuplot
 
+total_io=`awk '{print $3-$2}' ${device}.offset_*_c.dat |sort -k 1 |uniq -c | awk 'BEGIN {  totol=0;} {total += $1;}  END {print total}'`
+
+awk '{print $3-$2}' ${device}.offset_*_r.dat |sort -k 1 |uniq -c |sort -nk 2 | awk -v total=$total_io '{print $2,$1*100.0/total}' > $device.iosize_r_freq
+awk '{print $3-$2}' ${device}.offset_*_w.dat |sort -k 1 |uniq -c |sort -nk 2 | awk -v total=$total_io '{print $2,$1*100.0/total}' > $device.iosize_w_freq
+
+cat ${device}.iosize_r_freq ${device}.iosize_w_freq |sort -nk 1 > ${device}.iosize_freq
+join -1 1 -2 1 -o 0,1.2,2.2 -e 0 -a1 -a2 $device.iosize_r_freq $device.iosize_w_freq > $device.iosize_freq 2>/dev/null
+
+echo "
+set terminal pngcairo enhanced font 'arial,10' fontscale 1.0 size 800, 600  
+set auto x   
+set yrange [0:100] 
+set ylabel \"% of Total I/O\"  
+set xlabel \"I/O Size\"      
+set title \"I/O Distribution by I/O Size \" 
+set style histogram rowstacked  
+set boxwidth 0.25 relative 
+set style fill transparent solid 0.5 noborder 
+set xtics rotate by 90   
+set grid  
+set ytics 5 
+set output '${device}_iosize_hist.png' 
+plot \"$device.iosize_freq\"  u 2:xticlabels(1)  with boxes ti \"read\" , ''  using 3:xticlabels(1) with boxes ti \"write\" 
+" | /usr/bin/gnuplot
