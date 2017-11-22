065ee3a0b64535e17dbfad0564147c04e33f16f2
Bean Li <beanli.coder@gmail.com>
2017-11-22 01:13:01 -0800

    add -D option for print timestamp

--- streamwriter/streamwriter.c
+++ streamwriter/streamwriter.c
@@ -45,6 +45,7 @@ int thread_num = 0 ;
 struct statistic summary ;
 struct worker_args* args_array = NULL ;
 int buffering = 0 ;
+int debug = 0 ;
 
 
 int initialize_statistic(struct statistic* stat)
@@ -56,7 +57,7 @@ int initialize_statistic(struct statistic* stat)
     int i = 0 ;
     for (i= 0 ; i < N_HIST; i++)
     {
-        stat->hist[i] = 0;
+	stat->hist[i] = 0;
     }
 
     return 0;
@@ -71,15 +72,15 @@ int print_hist(int* hist)
     fprintf(stderr,"%20s:\t%10s\n","time-range (ms)", "times");
     for(j = 0; j < N_HIST; j++)
     {
-        if(j != N_HIST -1)
-        {
-            snprintf(header, 256, "[%5d~%6d]",interval[j], interval[j+1]);
-        }
-        else
-        {
-            snprintf(header, 256, "[%5d~%6s]",interval[j], "N/A");
-        }
-        fprintf(stderr,"%20s:\t%10d\n", header, hist[j]);
+	if(j != N_HIST -1)
+	{
+	    snprintf(header, 256, "[%5d~%6d]",interval[j], interval[j+1]);
+	}
+	else
+	{
+	    snprintf(header, 256, "[%5d~%6s]",interval[j], "N/A");
+	}
+	fprintf(stderr,"%20s:\t%10d\n", header, hist[j]);
     }
     fprintf(stderr,"----------------------------------------------\n\n");
     return 0;
@@ -87,7 +88,7 @@ int print_hist(int* hist)
 
 
 int print_header(int thread_num, int speed, int frame, int filesize, 
-                 int filenum, int begin_index)
+	int filenum, int begin_index)
 {
     fprintf(stdout,"============SUMMARY:===========\n");
     fprintf(stdout,"parallel             : %d\n",thread_num);
@@ -120,14 +121,14 @@ static void print_result_and_exit()
     int j ; 
     for(idx = 0 ; idx < thread_num; idx++)
     {
-        summary.frames += args_array[idx].stat->frames;
-        summary.drop_frame += args_array[idx].stat->drop_frame;
-        summary.total_time += args_array[idx].stat->total_time;
+	summary.frames += args_array[idx].stat->frames;
+	summary.drop_frame += args_array[idx].stat->drop_frame;
+	summary.total_time += args_array[idx].stat->total_time;
 
-        for(j=0; j < N_HIST;j++)
-        {
-            summary.hist[j] += args_array[idx].stat->hist[j];
-        }
+	for(j=0; j < N_HIST;j++)
+	{
+	    summary.hist[j] += args_array[idx].stat->hist[j];
+	}
     }
     print_summary(&summary);
 }
@@ -142,23 +143,23 @@ int r_write(int fd, char* buffer, ssize_t size)
     int write_bytes = 0;
     while(reserve_bytes > 0)
     {
-        write_bytes = write(fd,buffer, reserve_bytes);
-        if(write_bytes >= 0)
-        {
-            reserve_bytes -= write_bytes;
-            buffer += write_bytes;
-        } 
-        else if(write_bytes <0 && errno != EINTR)
-        {
-            fprintf(stderr, "error happened when write");
-            return -1;
-        }
+	write_bytes = write(fd,buffer, reserve_bytes);
+	if(write_bytes >= 0)
+	{
+	    reserve_bytes -= write_bytes;
+	    buffer += write_bytes;
+	} 
+	else if(write_bytes <0 && errno != EINTR)
+	{
+	    fprintf(stderr, "error happened when write");
+	    return -1;
+	}
     }
 
     if (reserve_bytes == 0)
-        return size;
+	return size;
     else
-        return -1;
+	return -1;
 
 }
 
@@ -170,6 +171,19 @@ unsigned long long time_us()
     return (unsigned long long)(tv.tv_sec*1000000 + tv.tv_usec);
 }
 
+void print_timestamp(unsigned long long time_us)
+{
+    time_t tv_sec = time_us /1000000 ; 
+    int  tv_msec = (time_us % 1000000)/1000 ;
+
+    struct tm tm_info ; 
+    localtime_r(&tv_sec, &tm_info);
+
+    char buffer[26] ; 
+
+    strftime(buffer,26, "%Y-%m-%d %H:%M:%S", &tm_info);
+    fprintf(stdout, "%s.%3d  FINISH ONE FRAME\n", buffer,tv_msec);
+}
 void * worker(void* param)
 {
     struct worker_args* args = param;
@@ -187,7 +201,7 @@ void * worker(void* param)
 
     int buf_size = speed/frames;
     if (buf_size >1024*1024)
-        buf_size = 1024*1024;
+	buf_size = 1024*1024;
 
     char* buffer = (char*)malloc(buf_size);
     memset(buffer, 0 , buf_size);
@@ -211,18 +225,18 @@ void * worker(void* param)
     int ret ; 
     if (strlen(workdir) != 0)
     {
-        ret = chdir(workdir);
-        if(ret !=0)
-        {
-            fprintf(stderr,"failed to change work dir to %s\n",workdir);
-            return NULL;
-        }
+	ret = chdir(workdir);
+	if(ret !=0)
+	{
+	    fprintf(stderr,"failed to change work dir to %s\n",workdir);
+	    return NULL;
+	}
     }
 
     for(i = 0; i < filenum ; i++)
     {
-        memset(filename,0,256);
-        snprintf(filename, 256,"%d-%d-%d.mp4",begin_index, index, i);
+	memset(filename,0,256);
+	snprintf(filename, 256,"%d-%d-%d.mp4",begin_index, index, i);
 	if (buffering == 0)
 	{
 	    fd = open(filename, O_CREAT|O_RDWR, S_IRWXU|S_IRGRP|S_IROTH);
@@ -242,79 +256,84 @@ void * worker(void* param)
 	    }
 	}
 
-        time_slice = (1000000/frames);
+	time_slice = (1000000/frames);
 
-        length = 0;
-        drop_frame = 0;
-        total_time = 0; 
-        loop = 0 ;
-        for(j = 0 ; j<N_HIST; j++)
-        {
-            hist[j] = 0;    
-        }
+	length = 0;
+	drop_frame = 0;
+	total_time = 0; 
+	loop = 0 ;
+	for(j = 0 ; j<N_HIST; j++)
+	{
+	    hist[j] = 0;    
+	}
 
-        while(length < filesize)
-        {
-            tm_begin = time_us();
+	while(length < filesize)
+	{
+	    tm_begin = time_us();
 
-            if(filesize - length > buf_size)
-                current_size = buf_size;
-            else
-                current_size = filesize - length;
+	    if(filesize - length > buf_size)
+		current_size = buf_size;
+	    else
+		current_size = filesize - length;
 
 	    if(buffering == 0)
 	    {
-                write_bytes = r_write(fd, buffer, current_size);
+		write_bytes = r_write(fd, buffer, current_size);
 	    }
 	    else
 	    {
-	        write_bytes = fwrite(buffer,1,current_size,fp);
+		write_bytes = fwrite(buffer,1,current_size,fp);
 	    }
-            if(write_bytes < 0 || write_bytes != current_size)
-            {
-                fprintf(stderr,"failed to write %s (%zd)",filename, length);
-                pthread_exit(NULL);
-            }
-            else
-            {
-                length += write_bytes;
-            }
-            tm_end = time_us();
-            loop++;
-            stat->frames++ ;
+	    if(write_bytes < 0 || write_bytes != current_size)
+	    {
+		fprintf(stderr,"failed to write %s (%zd)",filename, length);
+		pthread_exit(NULL);
+	    }
+	    else
+	    {
+		length += write_bytes;
+	    }
+	    tm_end = time_us();
+	    loop++;
+	    stat->frames++ ;
 
-            current_us = tm_end - tm_begin;
-            total_time += current_us;
-            stat->total_time += current_us;
+	    if(debug == 1)
+	    {
+	        print_timestamp(tm_end);
+	    }
 
-            for(j = 1; j < N_HIST ; j++)
-            {
-                if(current_us < interval[j]*1000) // ms to us
-                {
-                    hist[j-1]++; 
-                    stat->hist[j-1]++;
-                    break;
-                }
-            }
-            if(j == N_HIST)
-            {
-                hist[j-1]++;
-                stat->hist[j-1]++;
-            }
+	    current_us = tm_end - tm_begin;
+	    total_time += current_us;
+	    stat->total_time += current_us;
 
-            if(tm_end - tm_begin < time_slice)
-            {
-                sleep_time_us = time_slice - (tm_end - tm_begin);
-                usleep(sleep_time_us);
-            }
-            else
-            {
-                drop_frame++;
-                stat->drop_frame++;
-            }
-        }
+	    for(j = 1; j < N_HIST ; j++)
+	    {
+		if(current_us < interval[j]*1000) // ms to us
+		{
+		    hist[j-1]++; 
+		    stat->hist[j-1]++;
+		    break;
+		}
+	    }
+	    if(j == N_HIST)
+	    {
+		hist[j-1]++;
+		stat->hist[j-1]++;
+	    }
+
+	    if(tm_end - tm_begin < time_slice)
+	    {
+		sleep_time_us = time_slice - (tm_end - tm_begin);
+		usleep(sleep_time_us);
+	    }
+	    else
+	    {
+		drop_frame++;
+		stat->drop_frame++;
+	    }
+	}
 	if(buffering == 0)
-            close(fd);
+	    close(fd);
 	else
 	    fclose(fp);
     }
@@ -333,6 +352,7 @@ void usage()
     fprintf(stdout, "-S   --size       file size of each file  (MB)\n");
     fprintf(stdout, "-b   --prefix     file name's prefix \n");
     fprintf(stdout, "-B   --buffering  use fwrite instead of write \n");
+    fprintf(stdout, "-D   --debug      print timestamp when finish one frame \n");
 }
 
 
@@ -340,19 +360,20 @@ void usage()
 int main(int argc, char* argv[])
 {
     static struct option long_options[] = {
-        {"workdir",        required_argument, 0, 'd'},
-        {"thread_num",     required_argument, 0, 'p'},
-        {"speed",          required_argument, 0, 's'},
-        {"size",           required_argument, 0, 'S'},
-        {"fnum",           required_argument, 0, 'n'},
-        {"frame",          required_argument, 0, 'f'},
-        {"prefix",         required_argument, 0, 'b'},
-        {"buffering",      no_argument,       0, 'B'},
-        {0,                0 ,                0, 0  }
+	{"workdir",        required_argument, 0, 'd'},
+	{"thread_num",     required_argument, 0, 'p'},
+	{"speed",          required_argument, 0, 's'},
+	{"size",           required_argument, 0, 'S'},
+	{"fnum",           required_argument, 0, 'n'},
+	{"frame",          required_argument, 0, 'f'},
+	{"prefix",         required_argument, 0, 'b'},
+	{"buffering",      no_argument,       0, 'B'},
+	{"debug",          no_argument,       0, 'D'},
+	{0,                0 ,                0, 0  }
     };
 
     int c;
-    const char* short_options = "h?d:p:s:f:S:n:b:B";
+    const char* short_options = "h?d:p:s:f:S:n:b:BD";
 
     int option_index = 0;
 
@@ -370,65 +391,68 @@ int main(int argc, char* argv[])
     memset(workdir,'\0', PATH_MAX+1);
     while((c = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1)
     {
-        switch(c)
-        {
-        case 'd':
-            res = realpath(optarg, workdir);
-            if(!res)
-            {
-                fprintf(stderr, "failed to get realpath for %s\n", optarg);
-                exit(1);
-            }
+	switch(c)
+	{
+	case 'd':
+	    res = realpath(optarg, workdir);
+	    if(!res)
+	    {
+		fprintf(stderr, "failed to get realpath for %s\n", optarg);
+		exit(1);
+	    }
 
-            ret = stat(workdir,&statbuf);
-            if(ret !=0 )
-            {
-                fprintf(stderr,"failed to stat workdir %s\n", workdir);
-                exit(1);
-            }
-            if(!S_ISDIR(statbuf.st_mode))
-            {
-                fprintf(stderr, "workdir (%s) is not directory\n",workdir);
-                exit(1);
-            }
-            break;
-        case 'p':
-            thread_num = atoi(optarg);
-            break;
-        case 's':
-            speed = atoi(optarg); 
-            speed *= 1024 ; 
-            break;
-        case 'n':
-            file_num = atoi(optarg); 
-            break;
-        case 'S':
-            filesize = atoll(optarg); 
-            filesize = filesize*1024*1024 ;
-            break;
-        case 'f':
-            frame = atoi(optarg); 
-            break;
-        case 'b':
-            begin_index = atoi(optarg); 
-            break;
+	    ret = stat(workdir,&statbuf);
+	    if(ret !=0 )
+	    {
+		fprintf(stderr,"failed to stat workdir %s\n", workdir);
+		exit(1);
+	    }
+	    if(!S_ISDIR(statbuf.st_mode))
+	    {
+		fprintf(stderr, "workdir (%s) is not directory\n",workdir);
+		exit(1);
+	    }
+	    break;
+	case 'p':
+	    thread_num = atoi(optarg);
+	    break;
+	case 's':
+	    speed = atoi(optarg); 
+	    speed *= 1024 ; 
+	    break;
+	case 'n':
+	    file_num = atoi(optarg); 
+	    break;
+	case 'S':
+	    filesize = atoll(optarg); 
+	    filesize = filesize*1024*1024 ;
+	    break;
+	case 'f':
+	    frame = atoi(optarg); 
+	    break;
+	case 'b':
+	    begin_index = atoi(optarg); 
+	    break;
 	case 'B':
 	    buffering = 1;
 	    break;
-        case 'h':
-        case '?':
-            usage();
-            return 0;
-            break;
-        default:
-            break;
-        }
+	case 'D':
+	    debug = 1;
+	    break;
+	case 'h':
+	case '?':
+	    usage();
+	    return 0;
+	    break;
+	default:
+	    break;
+	}
     }
 
     if(speed <=0 || file_num <= 0 || filesize <=0 ||frame <=0)
     {
-        usage();
-        exit(1);
+	usage();
+	exit(1);
     }
 
     int idx = 0 ; 
@@ -438,20 +462,20 @@ int main(int argc, char* argv[])
 
     for(idx = 0 ; idx < thread_num; idx++)
     {
-        args_array[idx].filesize = filesize;
-        args_array[idx].p_workdir = workdir;
-        args_array[idx].filenum = file_num;
-        args_array[idx].begin_index = begin_index;
-        args_array[idx].speed = speed;
-        args_array[idx].frames = frame;
-        args_array[idx].index = idx;
-        args_array[idx].stat = (struct statistic*) malloc(sizeof(struct statistic));
+	args_array[idx].filesize = filesize;
+	args_array[idx].p_workdir = workdir;
+	args_array[idx].filenum = file_num;
+	args_array[idx].begin_index = begin_index;
+	args_array[idx].speed = speed;
+	args_array[idx].frames = frame;
+	args_array[idx].index = idx;
+	args_array[idx].stat = (struct statistic*) malloc(sizeof(struct statistic));
     }
 
 
     for(idx = 0 ; idx < thread_num; idx++)
     {
-        pthread_create(&(tids[idx]),NULL, worker, (void*)&(args_array[idx]));
+	pthread_create(&(tids[idx]),NULL, worker, (void*)&(args_array[idx]));
     }
 
     struct sigaction new_action, old_action;
@@ -465,7 +489,7 @@ int main(int argc, char* argv[])
     print_header(thread_num,speed,frame,filesize, file_num,begin_index);
     for(idx = 0 ; idx<thread_num; idx++)
     {
-        pthread_join(tids[idx],NULL);
+	pthread_join(tids[idx],NULL);
     }
 
 
